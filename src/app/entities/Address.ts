859335b78a68e001312a67912ee21bad3623d185
Adithya Vinod <adithyavinod@gmail.com>
2022-04-09 11:48:09 +0530

    Assignment Completed

---
+++ src/app/entities/Address.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AbstractEntity } from "./AbstractEntity";
+
+@Entity("address")
+export class Address extends AbstractEntity{
+    @PrimaryGeneratedColumn("uuid")
+    public id : string;
+    @Column({nullable: false})
+    public address : string;
+}
